92dcd991fbfd2eb8d461716534b99bcdd924531d
MasseyIsaako <lasiisaako@gmail.com>
2017-08-04 10:50:44 +1200

    Added age histogram

--- js/script.js
+++ js/script.js
@@ -2,13 +2,10 @@ var i = 0;
 var data, chart;
 var options = {}
 
-google.charts.load("current", {"packages": ["geochart"], "mapsApiKey":"AIzaSyDXAYAWFC-XzhLB2WKPA24xEdvcEnZW6AM"});
+google.charts.load("current", {"packages": ["geochart", "corechart"], "mapsApiKey":"AIzaSyDXAYAWFC-XzhLB2WKPA24xEdvcEnZW6AM"});
 google.charts.setOnLoadCallback(drawChart);
 
 function drawChart(){
-	var data = new google.visualization.DataTable();
-	data.addColumn("string", "Favourite Travel Destination");
-
 	$.ajax({
 		url: "js/clashboard.json",
 		beforeSend: function(xhr){
@@ -19,13 +16,43 @@ function drawChart(){
 		contentType: "application/json",
 		dataType: "json",
 		success: function(DataFromJSON){
-			chart = new google.visualization.GeoChart(document.getElementById("studentTravel"));
-			
+			// Travel Map
+			function travel(){
+				data = new google.visualization.DataTable();
+				data.addColumn("string", "Student Favourite Travel Destination");
+
 				for (var i = 0; i < DataFromJSON.length; i++) {
 					data.addRow([DataFromJSON[i].travel]);
 				}
-			
-			chart.draw(data, options);
+
+				options = {
+					datalessRegionColor: "#ebebeb",
+					defaultColor: "#125078",
+					backgroundColor: "#FFFFFF",
+				}
+
+				chart = new google.visualization.GeoChart(document.getElementById("studentTravel"));
+				chart.draw(data, options);
+			}
+
+			// Age Chart
+			function age(){
+				data = new google.visualization.DataTable();
+				data.addColumn("number", "Student Age");
+
+				for (var i = 0; i < DataFromJSON.length; i++) {
+					data.addRow([DataFromJSON[i].age]);
+				}
+
+				options = {}
+				
+				chart = new google.visualization.Histogram(document.getElementById("studentAge"));
+				chart.draw(data, options);
+			}
+
+			// Calling the functions
+			travel();
+			age();
 
 		}, error: function(){
 			console.log("bad");
